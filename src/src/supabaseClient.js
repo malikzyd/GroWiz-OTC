aabb7135528c3a3fc85b8b612e020f267ce542d0
malikzyd <malikzyd@gmail.com>
2026-09-10 16:17:53 +0400

    Delete src/src/supabaseClient.js

--- src/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'YOUR_PROJECT_URL_HERE'
-const supabaseKey = 'YOUR_ANON_PUBLIC_KEY_HERE'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
